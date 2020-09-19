b5877a56341a5bd65c659601341121f99529ab54
George Griffiths <george.griffiths@ibm.com>
2020-09-19 18:16:13 +0100

    persistent theme switcg

--- src/assets/index.js
+++ src/assets/index.js
@@ -17,11 +17,36 @@ mainContainer.thresholds = [0.7,0.3];
 
 var checkbox = document.querySelector('#theme');
 
+const toggleTheme = () => {
+    const themePreference = localStorage.getItem("theme-switch-on");
+    if(themePreference) {
+        // toggle the theme switcher on, depending on their OS settings for prefs could be light or dark
+        // or if matchMedia not supported on = dark
+        checkbox.checked = true
+    } else {
+        checkbox.checked = false;
+    }
+}
+toggleTheme();
+
+// for multi tabs
+window.addEventListener('storage', () => {
+    toggleTheme();
+})
+
 checkbox.addEventListener('change', function() {
+    let defaultTheme = 'light';
+    const darkModeMediaQuery = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)');
+    if(darkModeMediaQuery && darkModeMediaQuery.matches) {
+        defaultTheme = 'dark';
+    }
     if(this.checked) {
+        // remember preference
+        localStorage.setItem("theme-switch-on", true);
         trans()
         // todo make it save and respect preferences!
     } else {
+        localStorage.removeItem("theme-switch-on");
         trans()
     }
 })
