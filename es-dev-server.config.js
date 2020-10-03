57666ff778cc9b27015066ae968357362d665d97
George Griffiths <george.griffiths@ibm.com>
2020-10-03 13:23:01 +0100

    font-fixes

--- es-dev-server.config.js
+++ es-dev-server.config.js
@@ -1,6 +1,7 @@
 // npm start -> concurrently \"npx gulp watch\" \"npx eleventy --watch\" \"es-dev-server\"
 const path = require("path");
 const fs = require("fs").promises;
+const URL = require("url").URL;
 /**
  *
  * Check if asset lives in 11ty dist folder, if not serve from root folder.
@@ -8,11 +9,12 @@ const fs = require("fs").promises;
 const serve11tyAssets = ({dist = "_site"} = {}) => {
     return async (context, next) => {
         // is the request for a html file?
-        const url = context.url.endsWith("/") ? `${context.url}index.html` : context.url;
+        const pathName = new URL(`https://whatever.com${context.url}`).pathname;
+        const url = pathName.endsWith("/") ? `${pathName}index.html` : pathName;
         try {
             const stats = await fs.stat(path.join(dist, url));
             if (stats.isFile()) {
-                context.url = `/${dist}${context.url}`
+                context.url = `/${dist}${pathName}`
             }
             return next();
         } catch  {
