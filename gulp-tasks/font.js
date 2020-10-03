efa07e8819549cfd3c56964e5b7c6e03a13e01f9
George Griffiths <george.griffiths@ibm.com>
2020-10-03 13:50:19 +0100

    inline fonts

--- gulp-tasks/font.js
+++ gulp-tasks/font.js
@@ -1,19 +1,36 @@
 const {dest, src} = require('gulp');
 const GetGoogleFonts = require('get-google-fonts');
 
+const font = 'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;800&display=swap';
+
 const fonts = async () => {
   // Setup of the library instance by setting where we want
   // the output to go. CSS is relative to output font directory
   const instance = new GetGoogleFonts({
     outputDir: './dist/fonts',
+    overwriting: true,
     cssFile: './fonts.css'
   });
 
+  const instanceCssFile = new GetGoogleFonts({
+    outputDir: './src/_includes/fonts',
+    path: "./fonts/",
+    overwriting: true,
+    cssFile: './fonts.css'
+  });
+
+
   try  {
     // Grabs fonts and CSS from google and puts in the dist folder
-    const result = await instance.download(
+    await instance.download(
       // add new weights as needed
-      'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;800&display=swap'
+      font
+      
+    );
+    await instanceCssFile.download(
+      // add new weights as needed
+      font
+      
     );
   } catch(e) {
     return  null;
